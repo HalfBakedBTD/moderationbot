8ebd9a848ef3195f0b2856c395d3c173312a58af
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-05-01 19:39:08 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+exports.run = async (bot, message, args) => {
+  let botEmbed = new Discord.RichEmbed()
+  .setColor('#fff200')
+  .setTitle("Bot Commands:")
+  .setDescription("`-help` - shows help message.");
+  message.channel.send(botEmbed)
+}
+	      
+module.exports.help = {
+  name: "help"
+}
