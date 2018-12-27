2b11a8e45b70a78d661337721f722f346d908ebf
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-12-27 18:20:34 -0500

    Delete server.js

--- commands/server.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-
-exports.run = async (bot, message, args) => {
-  message.channel.send("My Server: https://discord.gg/csCXCZe")
-}
-
-module.exports.help = {
-  name: "server"
-}
