213ccca091065217346d26e962e662385b2fb747
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-12-27 18:22:44 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ exports.run = async (bot, message, args) => {
   let botEmbed = new Discord.RichEmbed()
   .setColor('#fff200')
   .setTitle("Bot Commands:")
-  .setDescription("`-help` shows help message.\n`-invite` sends a link to add me to your server.\n`-server` makes me send you a link to my owner's server.\n\n`-ban` bans a user.\n`-kick` kicks a user.\n`-mute` mutes a user for a set amount of time\n`-clear` deletes a set amount of messages.\n\nThis bot was made by <@284137818895417344> from Liquish Setups. To order your own bot or website, you can find us [here](https://discord.gg/uKgPWjP)!");
+  .setDescription("`.help` shows help message.\n***MORE COMMING SOON!***");
   message.channel.send(botEmbed)
 }
 	      
